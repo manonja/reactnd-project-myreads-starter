4603caf63449d3ef1e551eef23501242acbee38b
Maja <manon.jacquin@gmail.com>
2018-03-09 17:07:34 -0300

    Adds BookSearchBar component to BooksApp component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import BookShelf from './BookShel.js'
+import BookShelf from './BookShelf.js'
 import Book from './Book.js'
+import BookSearchBar from './BookSearchBar.js'
+
 
 
 class BooksApp extends React.Component {
@@ -40,26 +42,7 @@ class BooksApp extends React.Component {
       <div className="app">
       // If showSearchPage is true, show bookSearchPage
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+            BookSearchBar
           // else: show the bookShelf
         ) : (
           <div className="list-books">
