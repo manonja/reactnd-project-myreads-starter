2088b733dc743fa8bd3060d4bbfac13f09842bd8
Maja <manon.jacquin@gmail.com>
2018-03-09 16:42:04 -0300

    Creates const book and const shelf to pass props

--- src/Book.js
+++ src/Book.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 
 class Book extends Component {
 
+
     render(){
+        const book = this.props.book;
+        const shelf = this.props.shelf;
+
         return(
-            <div className="bookshelf-books">
-              <ol className="books-grid">
+
                 <li>
                   <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                      <div className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="none" disabled>Move to...</option>
@@ -24,8 +31,7 @@ class Book extends Component {
                     <div className="book-authors">Harper Lee</div>
                   </div>
                   </li>
-              </ol>
-            </div>
+
         )
     }
 
