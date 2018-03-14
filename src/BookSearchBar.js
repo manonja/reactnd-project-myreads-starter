56661c8a897920dded4707ecb1420c53a5a47c31
Maja <manon.jacquin@gmail.com>
2018-03-14 15:04:17 -0300

    Adds Link component and set it to /

--- src/BookSearchBar.js
+++ src/BookSearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from "./Book";
 
@@ -30,7 +31,11 @@ class BookSearchBar extends React.Component {
         return (
             <div className="search-books">
               <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                <Link
+                    to="/"
+                    className="close-search"
+                    onClick={() => this.setState({ showSearchPage: false })}>Close
+                </Link>
                 <div className="search-books-input-wrapper">
                   {/*
                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
