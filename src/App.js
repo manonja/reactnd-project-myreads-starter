10ba4ad3a1b441f1d21787a6361752b2ec0a9ed9
Maja <manon.jacquin@gmail.com>
2018-03-16 11:12:22 -0300

    Updates state, creates bookList method and includes new component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import BookShelf from './BookShelf'
 import SearchBar from './SearchBar.js'
+import BookList from './BookList.js'
+import { Route } from 'react-router-dom'
 
 
 class BooksApp extends React.Component {
   state={
     //Empty array that will store the books being called by the BooksAPI.getAll()
     books: [],
-
-    //BookShelf object representing the app structure
-    shelves: [
-        {
-            id: 'currentlyReading',
-            name: 'Currently Reading'
-        },
-        {
-            id: 'wantToRead',
-            name: 'Want to Read'
-
-        },
-        {
-            id: 'read',
-            name: 'Read'
-        }
-    ],
-
-    showSearchPage: false
+    search:[],
 }
 
   //Adds life cycle event - Fetching infos from BooksApi.js to retrieve books
   componentDidMount() {
-      BooksAPI.getAll().then((books) => {
+      BooksAPI.getAll().then(books => {
           this.setState({ books })
       })
   }
@@ -45,59 +26,44 @@ class BooksApp extends React.Component {
    * @param {object} shelfValue - The shelf 's target value where the book needs to move to.
    * @returns {array} - new array with the new value of state.books
   */
-  moveBooks=(bookToMove, shelfValue) => {
 
-     this.setState(state => {
-         //Filter the selected book within books array
-         const newShelf=state.books.filter(book => book.id !== bookToMove.id);
-         //Update shelf value
-         bookToMove.shelf = shelfValue
-         //Returns a new array with new book and shelf values
-         return {
-           books: newShelf.concat(bookToMove)
-       };
-     });
-     //Update the database
-     BooksAPI.update(bookToMove, shelfValue);
- };
+  moveBooks = (book, shelf) => {
+      BooksAPI.update(book, shelf).then(() => {
+          BooksAPI.getAll().then(books => {
+              this.setState({ books });
+          });
+      });
+  };
+
+  /**
+   * Passing a value, search the corresponding value in API, search results updated
+   * @param {string} query - new query to be checked for a new search
+   * @returns {object} - new object with the books search results
+  */
+
+  bookList = (query) => {
+    BooksAPI.search(query).then(books => this.setState({search:books}))
+  }
 
   render() {
+
     return (
       <div className="app">
         <Route
             path='/search'
             render={() => (
-                 <SearchBar moveBooks={this.moveBooks}/>
+                 <SearchBar bookList={this.bookList} books={this.state.search} moveBooks={this.moveBooks}/>
             )}
         />
 
         <Route
             exact path='/'
             render={() => (
-                <div className="list-books">
-                  <div className="list-books-title">
-                    <h1>MyReads</h1>
-                  </div>
-                  <div className="list-books-content">
-                      {//Map over shelves state and render BookShelf component
-                        this.state.shelves.map((shelf) => (
-                          <BookShelf
-                              shelf={shelf}
-                              key={shelf.id}
-                              books={this.state.books.filter(book => book.shelf === shelf.id)}
-                              moveBooks={this.moveBooks}
-                          />
-                        ))
-                      }
-                  </div>
-                  <div className="open-search">
-                    <Link to="/search">Add a book</Link>
-                  </div>
-                </div>
+                <BookList books={this.state.books} moveBooks={this.moveBooks} />
                )}
               />
             }
-        </div>
+      </div>
     )
   }
 }
